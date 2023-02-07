d4cb5a30bac746b4f7879a3f2557c687dfe4ef8e
farahjerbi <farah.jerbi@esprit.tn>
2023-02-07 15:32:30 +0100

    Titre

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+const logDateTime= (req,res,next)=>{
+    req.dateTime=new Date();
+    console.log(new Date());
+    next();
+};
+
+const requireJsonContent = () => {
+    return (req, res, next) => {
+    if(req.headers['content-type'] !== 'application/json') {
+    res.status(400).send('Server requires application/json');
+    } else {
+    next();
+    }}}
+
+app.use(logDateTime);
+
+app.get('/' ,middle, (req,res,next)=>{
+    res.send (`Hello :)  ${req.dateTime}`);
+});
+
+
+app.listen(port ,()=>console.log(`server is running on port:${port}`));
